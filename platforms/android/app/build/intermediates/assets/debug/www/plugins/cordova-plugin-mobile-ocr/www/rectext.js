95d0bad266a501ddfd40b681ad180cd26b0909eb
Cristian Rinaldi <csrinaldi@gmail.com>
2018-10-11 21:19:55 -0300

    Eliminacion de platforms

--- platforms/android/app/build/intermediates/assets/debug/www/plugins/cordova-plugin-mobile-ocr/www/rectext.js
+++
@@ -1,9 +0,0 @@
-cordova.define("cordova-plugin-mobile-ocr.rectext", function(require, exports, module) {
-/*global cordova, module*/
-
-module.exports = {
-    recText: function (sourceType, returnType, imageSource, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Textocr", "recText", [sourceType, returnType, imageSource]);
-    }
-};
-});
--- platforms/android/app/src/main/assets/www/js/rectext.js
+++
@@ -1,7 +0,0 @@
-/*global cordova, module*/
-
-module.exports = {
-    recText: function (sourceType, returnType, imageSource, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Textocr", "recText", [sourceType, returnType, imageSource]);
-    }
-};
